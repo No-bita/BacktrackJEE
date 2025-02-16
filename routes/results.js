104ef00493b75b4c35146bc1159cf59e9963f6e0
Shah Aaryan <78593044+No-bita@users.noreply.github.com>
2025-02-16 17:51:58 +0530

    sc

--- routes/results.js
+++ routes/results.js
@@ -41,7 +41,7 @@ router.get('/calculate', authenticateUser, async (req, res) => {
                     const objectId = new mongoose.Types.ObjectId(questionId);
 
                     // Dynamically determine the correct collection
-                    const collectionName = `${year}_${slot.replace(/\s+/g, "_")}_questions`.toLowerCase();
+                    const collectionName = `${slot.replace(/\s+/g, "_")}`.toLowerCase();
                     const QuestionModel = mongoose.models[collectionName] || mongoose.model(collectionName, new mongoose.Schema({}, { strict: false, collection: collectionName }));
 
                     // Fetch the question document
